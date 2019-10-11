5742d841307be301bfb663dfe9d82e592660eb20
Gang Sun <sun_jungang@hotmail.com>
2019-10-11 15:10:40 +0800

    update esclu

--- esclu/Esclu.hx
+++ esclu/Esclu.hx
@@ -10,14 +10,7 @@ class Esclu {
           .description('Elasticsearch Command Line Utilities')
           .usage('[options] <command> [...]')
           .option('-o, --host <hostname>', 'hostname [localhost]', 'localhost')
-          .option('-p, --port <number>', 'port number [9200]', '9200')
-          .option('-j, --json', 'format output as JSON')
-          .option('-i, --index <name>', 'which index to use')
-          .option('-t, --type <type>', 'default type for bulk operations');
-
-        program
-          // Other options...
-          .option('-f, --filter <filter>', 'source filter for query results');
+          .option('-p, --port <number>', 'port number [9200]', '9200');
 
         program
           .command('ping')
@@ -40,16 +33,49 @@ class Esclu {
           });
 
         program
-          .command('get [path]')
-          .description('perform an HTTP GET request for path (default is /)')
-          .action((path='/') -> {
-
+          .command('get <id>')
+          .description('get an entry')
+          .option('-i, --index <name>', 'which index to use')
+          .option('-t, --type <type>', 'default type for bulk operations')
+          .action((id, options) -> {
+                if (options.index == null || options.type == null) {
+                    console.error('no command given!');
+                    process.exit(1);
+                }
+                var client = new Client({
+                  host: '${program.host}:${program.port}',
+                  log: 'trace'
+                });
+                client.get({
+                    index: options.index,
+                    type: options.type,
+                    id: id
+                }, function(error, result) {
+                  if (error != null) {
+                      console.log('elasticsearch cluster is down');
+                  } else {
+                      console.log(result);
+                  }
+                });
           });
 
         program
-          .command('create-index')
+          .command('create-index <index>')
           .description('create an index')
-          .action(() -> {
+          .action((index) -> {
+                var client = new Client({
+                  host: '${program.host}:${program.port}',
+                  log: 'trace'
+                });
+                client.indices.create({
+                    index: index,
+                }, function(error, result) {
+                  if (error != null) {
+                      console.log('elasticsearch cluster is down');
+                  } else {
+                      console.log(result);
+                  }
+                });
           });
 
         program
@@ -57,25 +83,71 @@ class Esclu {
           .alias('li')
           .description('get a list of indices in this cluster')
           .action(() -> {
+                var client = new Client({
+                  host: '${program.host}:${program.port}',
+                  log: 'trace'
+                });
+                client.cat.indices({
+                    format: 'json'
+                }, function(error, result) {
+                  if (error != null) {
+                      console.log('elasticsearch cluster is down');
+                  } else {
+                      console.log(result);
+                  }
+                });
           });
 
         program
-          .command('delete-index')
+          .command('delete-index <index>')
           .description('delete an index')
-          .action(() -> {
+          .action((index) -> {
+                var client = new Client({
+                  host: '${program.host}:${program.port}',
+                  log: 'trace'
+                });
+                client.indices.delete({
+                    index: index,
+                }, function(error, result) {
+                  if (error != null) {
+                      console.log('elasticsearch cluster is down');
+                  } else {
+                      console.log(result);
+                  }
+                });
           });
 
         program
           .command('bulk <file>')
           .description('read and perform bulk options from the specified file')
           .action((file) -> {
+                console.log('file ${file}');
           });
 
         program
           .command('query [queries...]')
           .alias('q')
           .description('perform an Elasticsearch query')
-          .action((queries) -> {
+          .option('-i, --index <name>', 'which index to use')
+          .action((queries, options) -> {
+                if (queries == null) {
+                    console.error('no queries given!');
+                    process.exit(1);
+                }
+                var client = new Client({
+                  host: '${program.host}:${program.port}',
+                  log: 'trace'
+                });
+                client.search({
+                    index: options.index=null?'_all':options.index,
+                    q: queries.join(' ')
+                }, function(error, result) {
+                  if (error != null) {
+                      console.log('elasticsearch cluster is down');
+                  } else {
+                      console.log(result);
+                  }
+                });
           });
 
 
