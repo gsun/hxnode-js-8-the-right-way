a8442401f1c5aa1ede2a38de14c1068098a8d103
Gang Sun <sun_jungang@hotmail.com>
2019-10-12 12:02:02 +0800

    add morgan

--- web-services/HelloServer.hx
+++ web-services/HelloServer.hx
@@ -1,9 +1,11 @@
 import js.npm.Express;
+import js.npm.express.Morgan;
 import js.Node.console;
 
 class HelloServer {
     static function main() {
         var app = new Express();
+        app.use(new Morgan(MorganFormat.dev));
         
         app.get('/hello/:name', (req, res) -> {
             res.status(200).json({'hello': req.params.name});
