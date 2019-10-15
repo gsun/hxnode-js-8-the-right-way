87cbd77bfcf48d8389f4e3f1d97c98dbb2d42c2c
Gang Sun <sun_jungang@hotmail.com>
2019-10-15 15:38:27 +0800

    add B4Server with Request

---
+++ web-services/B4ServerRequest.hx
@@ -0,0 +1,49 @@
+import js.npm.Express;
+import js.npm.express.Morgan;
+import js.Node.console;
+import js.npm.NodeEnvFile;
+import js.Node.process;
+import js.lib.Error;
+import js.npm.Request;
+import js.lib.Promise;
+using Lambda;
+
+class B4ServerRequest {
+    static function main() {
+        try {
+            new NodeEnvFile(js.Node.__dirname + '/.env');
+        } catch(e : Error) {}
+        
+        var app = new Express();
+        app.use(new Morgan(MorganFormat.dev));
+        
+        app.get('/hello/:name', (req, res) -> {
+            res.status(200).json({'hello': req.params.name});
+        });
+        
+        app.get('/api/search/books/:field/:query', (req, res) -> {
+		    var path = 'http://${process.env["es_host"]}:${process.env["es_port"]}/${process.env["books_index"]}/_search';
+			var options = haxe.Json.parse('{
+                "url" : "${path}",
+                "josn" : "true",
+				"query" : {
+					"match" : {
+						"${req.params.field}" : "${req.params.query}"
+					}
+				}   
+            }');
+			Request.debug = true;
+			var r = Request.construct();
+			r.get(options, (err, esRes, esResBody) -> {
+			  if (err != null) {
+				res.status(502);
+				return;
+			  }
+			  var p:{hits:{hits:Array<{_source:Dynamic}>}} = haxe.Json.parse(esResBody);
+			  res.status(200).json([for (hit in p.hits.hits) {_source : hit._source}]);
+			});
+        });
+        
+        app.listen(Std.parseInt(process.env["port"]), () -> console.log('Ready.'));
+    }
+}
